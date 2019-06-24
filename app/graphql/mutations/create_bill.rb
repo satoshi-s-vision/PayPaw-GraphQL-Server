b0b815a0320cde4a2e40c44bae69e1ebde515b6b
Dalin Huang <dhuan023@gmail.com>
2019-06-24 01:23:12 -0400

    add mutation: bill

---
+++ app/graphql/mutations/create_bill.rb
@@ -0,0 +1,24 @@
+class Mutations::CreateBill < Mutations::BaseMutation
+  argument :note, String, required: true
+
+  field :bill, Types::BillType, null: false
+  field :errors, [String], null: false
+
+  def resolve(note: nil)
+    bill = Bill.new(note: note, user_id: context[:current_user].id)
+
+    if bill.save
+      # Successful creation, return the created object with no errors
+      {
+        bill: bill,
+        errors: [],
+      }
+    else
+      # Failed save, return the errors to the client
+      {
+        bill: nil,
+        errors: bill.errors.full_messages
+      }
+    end
+  end
+end
