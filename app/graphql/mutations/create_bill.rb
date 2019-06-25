6e5b7599789461c2abd7222358713374d2dc0381
Dalin Huang <dhuan023@gmail.com>
2019-06-24 23:05:09 -0400

    mutation update: bill

--- app/graphql/mutations/create_bill.rb
+++ app/graphql/mutations/create_bill.rb
@@ -1,14 +1,18 @@
 class Mutations::CreateBill < Mutations::BaseMutation
   argument :note, String, required: true
+  argument :currency, String, required: true
+  argument :currency_amount, Float, required: true
 
   field :bill, Types::BillType, null: false
   field :errors, [String], null: false
   field :currency_amount, Float, null: false
+  field :currency, String, null: false
 
-  def resolve(note: nil, currency_amount: 0)
+  def resolve(note: nil, currency: 'USD', currency_amount: 0.0)
     bill = Bill.new(
       note: note,
       user_id: context[:current_user].id,
+      currency: currency,
       currency_amount: currency_amount,
       address: SecureRandom.hex(21), # this is just for mock address
     )
--- app/graphql/types/bill_type.rb
+++ app/graphql/types/bill_type.rb
@@ -1,11 +1,15 @@
 module Types
   class BillType < Types::BaseObject
     field :id, ID, null: false
-    field :address, String, null: false
     field :note, String, null: false
-    def note
-      @object.user.email + '-' + @object.note
-    end
+    field :currency, String, null: false
+    field :currency_amount, Float, null: false
+    field :address, String, null: false
+    field :status, Boolean, null: false
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    # def note
+    #   @object.user.email + '-' + @object.note
+    # end
 
     field :user, UserType, null: false
 
