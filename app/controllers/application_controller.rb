f861c4a8b502a4a54d058bdd32f8ddd49d0f0667
Dalin Huang <dhuan023@gmail.com>
2019-06-27 00:07:26 -0400

    force SSL for production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+
+  force_ssl if: :ssl_configured?
+
+  private
+  def ssl_configured?
+    Rails.env.production?
+  end
+
 end
